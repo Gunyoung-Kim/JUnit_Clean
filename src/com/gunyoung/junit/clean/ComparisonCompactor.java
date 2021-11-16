b9e9aeeae6b1b717a8cf3e3e7a5a1abd2c4f9e16
Gunyoung-Kim <gun025bba@naver.com>
2021-11-16 20:35:38 +0900

    ComparisonCompactor: 멤버 면수 이름에 f 접두사 삭제

--- src/com/gunyoung/junit/clean/ComparisonCompactor.java
+++ src/com/gunyoung/junit/clean/ComparisonCompactor.java
@@ -6,53 +6,53 @@ public class ComparisonCompactor {
 	private static final String DELTA_END = "]";
 	private static final String DELTA_START = "[";
 	
-	private int fContextLength;
-	private String fExpected;
+	private int contextLength;
+	private String expected;
 	private String fActual;
 	private int fPrefix;
 	private int fSuffix;
 	
 	private ComparisonCompactor(int contextLength, String expected, String actual) {
 		super();
-		this.fContextLength = contextLength;
-		this.fExpected = expected;
+		this.contextLength = contextLength;
+		this.expected = expected;
 		this.fActual = actual;
 	}
 	
 	public String compact(String message) {
 		if(shouldNotCompact()) 
-			return Assert.format(message, fExpected, fActual);
+			return Assert.format(message, expected, fActual);
 		
 		findCommonPrefix();
 		findCommonSuffix();
-		String expected = compactString(fExpected);
+		String expected = compactString(this.expected);
 		String actual = compactString(fActual);
 		return Assert.format(message, expected, actual);
 	}
 	
 	private boolean shouldNotCompact() {
-		return fExpected == null || fActual == null || areStringEqual();
+		return expected == null || fActual == null || areStringEqual();
 	}
 	
 	private void findCommonPrefix() {
 		fPrefix = 0;
-		int end = Math.min(fExpected.length(), fActual.length());
+		int end = Math.min(expected.length(), fActual.length());
 		for(; fPrefix < end; fPrefix++) {
-			if(fExpected.charAt(fPrefix) != fActual.charAt(fPrefix))
+			if(expected.charAt(fPrefix) != fActual.charAt(fPrefix))
 				break;
 		}
 	}
 	
 	private void findCommonSuffix() {
-		int expectedSuffix = fExpected.length() - 1;
+		int expectedSuffix = expected.length() - 1;
 		int actualSuffix = fActual.length() - 1;
 		for(;
 			actualSuffix >= fPrefix && expectedSuffix >= fPrefix;
 			actualSuffix--, expectedSuffix--) {
-			if(fExpected.charAt(expectedSuffix) != fActual.charAt(actualSuffix))
+			if(expected.charAt(expectedSuffix) != fActual.charAt(actualSuffix))
 				break;
 		}
-		fSuffix = fExpected.length() - expectedSuffix;
+		fSuffix = expected.length() - expectedSuffix;
 	}
 	
 	private String compactString(String source) {
@@ -66,16 +66,16 @@ public class ComparisonCompactor {
 	}
 	
 	private String computeCommonPrefix() {
-		return (fPrefix > fContextLength ? ELLIPSIS : "") + fExpected.substring(Math.max(0, fPrefix - fContextLength), fPrefix);
+		return (fPrefix > contextLength ? ELLIPSIS : "") + expected.substring(Math.max(0, fPrefix - contextLength), fPrefix);
 	}
 	
 	private String computeCommonSuffix() {
-		int end = Math.min(fExpected.length() - fSuffix + 1 + fContextLength, fExpected.length());
-		return fExpected.substring(fExpected.length() - fSuffix + 1, end) + 
-				(fExpected.length() - fSuffix + 1 < fExpected.length() - fContextLength ? ELLIPSIS : "");
+		int end = Math.min(expected.length() - fSuffix + 1 + contextLength, expected.length());
+		return expected.substring(expected.length() - fSuffix + 1, end) + 
+				(expected.length() - fSuffix + 1 < expected.length() - contextLength ? ELLIPSIS : "");
 	}
 	
 	private boolean areStringEqual() {
-		return fExpected.equals(fActual);
+		return expected.equals(fActual);
 	}
 }
